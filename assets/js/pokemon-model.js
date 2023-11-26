b2a48079afcf2855c1e704ecc33a1694a099da40
Jorge Guedes <jorge_guedes00@hotmail.com>
2023-11-26 13:33:03 -0300

    feat: add new pokemon model stats

--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -1,8 +1,18 @@
-
 class Pokemon {
-    number;
-    name;
-    type;
-    types = [];
-    photo;
+  number;
+  name;
+  type;
+  types = [];
+  photo;
 }
+
+class BaseStats {
+  hp;
+  attack;
+  defense;
+  specialAttack;
+  specialDefense;
+  speed;
+}
+
+class PokemonCard extends BaseStats {}
